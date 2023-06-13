8c35bcf857852d6e9f9b5573df041c2f20eb0ee9
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-06-13 13:26:48 +0300

    Improve toggle button style

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -19,10 +19,9 @@ const Togglable = forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <button style={{ padding: '0.5rem 0.75rem', lineHeight: 'inherit'}} onClick={toggleVisibility}>{props.buttonLabel}</button>
       </div>
       <div style={showWhenVisible}>
-
         {props.children}
         <button onClick={toggleVisibility}>cancel</button>
       </div>
