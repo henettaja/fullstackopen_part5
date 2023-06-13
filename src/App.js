94e5f8d92f4f4e6bb2e892248c30571094b728a2
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-06-13 13:27:17 +0300

    Improve HTML semantics

--- src/App.js
+++ src/App.js
@@ -98,11 +98,17 @@ const App = () => {
         </Togglable>
 
         <h2>Blogs</h2>
-        {
-          blogs.map((blog) =>
-            <Blog key={blog.id} blog={blog} />
-          )
-        }
+        <ul style={
+          {
+            listStyleType: 'none',
+          }
+        }>
+          {
+            blogs.map((blog) =>
+              <Blog key={blog.id} blog={blog} />
+            )
+          }
+        </ul>
       </div>
     )
   }
